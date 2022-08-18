b27828600fc1aa3c1c794b566aa4219b9e840a42
xstartstaticx <daniel.reiller@icloud.com>
2022-08-18 08:58:46 +0200

    finished exercise

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-
 //Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
 //1
 
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
-function groupById(array) {
-  //write your code here
+// function groupById(array) {
+//   //write your code here
+//   const newUsers = array.reduce((nameGroup, person) => {
+//     const id = person.id;
+//     console.log(nameGroup);
+//     if (nameGroup[id] === undefined) {
+//       nameGroup[id] = [];
+//       nameGroup[id].push(person);
+//     }
+//     return nameGroup;
+//   }, {});
+// }
+// function groupByID(users) {
+//   users.reduce((usersById, user) => {
+//     if (usersById[user.id] === undefined)
+//     usersById[user.id] = user;
+//   }, {});
+// }
+function groupById(users) {
+  return users.reduce((usersById, user) => {
+    usersById[user.id] = user;
+    return usersById;
+  }, {});
 }
 
-let usersById= groupById(users)
-console.log(usersById)
+let usersById = groupById(users);
+console.log(usersById);
 
 /* expected output: 
 
